89fff6679eaa40dfa12b18891e58b22040d559bb
Thullyoo <thullyofilho@gmail.com>
2024-12-23 16:31:48 -0300

    fix: lombok bug getter and setter

--- back-end/src/main/java/br/thullyoo/desafio_uol/model/GrupoCodinome.java
+++ back-end/src/main/java/br/thullyoo/desafio_uol/model/GrupoCodinome.java
@@ -7,16 +7,28 @@ public enum GrupoCodinome {
     LIGA_DA_JUSTICA("Liga Da Justiça", "https://raw.githubusercontent.com/uolhost/test-backEnd-Java/master/referencias/liga_da_justica.xml"),
     VINGADORES("Vingadores", "https://raw.githubusercontent.com/uolhost/test-backEnd-Java/master/referencias/vingadores.json");
 
-    @Getter
-    @Setter
     private String nome;
 
-    @Getter
-    @Setter
     private String uri;
 
     GrupoCodinome(String nome, String uri){
         this.nome = nome;
+        this.uri = uri;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getUri() {
+        return uri;
+    }
+
+    public void setUri(String uri) {
         this.uri = uri;
     }
 }
--- back-end/src/main/java/br/thullyoo/desafio_uol/model/Jogador.java
+++ back-end/src/main/java/br/thullyoo/desafio_uol/model/Jogador.java
@@ -1,34 +1,71 @@
 package br.thullyoo.desafio_uol.model;
 
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
+@Data
 public class Jogador {
 
-    @Getter
-    @Setter
     private Long id;
 
-    @Getter
-    @Setter
     private String nome;
 
-    @Getter
-    @Setter
     private String email;
 
-    @Getter
-    @Setter
     private String telefone;
 
-    @Getter
-    @Setter
     private GrupoCodinome grupoCodinome;
 
-    @Getter
-    @Setter
     private String codinome;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public GrupoCodinome getGrupoCodinome() {
+        return grupoCodinome;
+    }
+
+    public void setGrupoCodinome(GrupoCodinome grupoCodinome) {
+        this.grupoCodinome = grupoCodinome;
+    }
+
+    public String getCodinome() {
+        return codinome;
+    }
+
+    public void setCodinome(String codinome) {
+        this.codinome = codinome;
+    }
 }
 
