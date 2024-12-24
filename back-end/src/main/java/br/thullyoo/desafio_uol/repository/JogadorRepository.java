82684a34842b4003c2c69e26f8c7640dfd79e0d5
Thullyoo <thullyofilho@gmail.com>
2024-12-24 16:06:51 -0300

    feat: add method to get codinomes used

--- back-end/src/main/java/br/thullyoo/desafio_uol/repository/JogadorRepository.java
+++ back-end/src/main/java/br/thullyoo/desafio_uol/repository/JogadorRepository.java
@@ -1,9 +1,13 @@
 package br.thullyoo.desafio_uol.repository;
 
+import br.thullyoo.desafio_uol.model.GrupoCodinome;
 import br.thullyoo.desafio_uol.model.Jogador;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 public class JogadorRepository {
@@ -19,10 +23,22 @@ public class JogadorRepository {
                 .param("nome", jogador.getNome())
                 .param("email", jogador.getEmail())
                 .param("telefone", jogador.getTelefone())
-                .param("grupo_codinome", jogador.getGrupoCodinome())
+                .param("grupo_codinome", jogador.getGrupoCodinome().getNome())
                 .param("codinome", jogador.getCodinome())
                 .update();
         return  jogador;
     }
 
+    public List<String> resgatarCodinomesUtilizados(GrupoCodinome grupoCodinome){
+
+        List<String> codinomes = new ArrayList<>();
+
+        codinomes = jdbcClient.sql("SELECT distinct(codinome) FROM JOGADOR WHERE grupo_codinome = :grupoCodinome")
+                .param("grupoCodinome", grupoCodinome.getNome())
+                .query(String.class)
+                .list();
+
+
+        return codinomes;
+    }
 }
