ab85a580dcfd3d32fd787d4d1d2d08b371044c03
Thullyoo <thullyofilho@gmail.com>
2024-12-26 11:27:12 -0300

    feat: add method to get jogadores

--- front-end/src/app/services/jogador.service.ts
+++ front-end/src/app/services/jogador.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import {Jogador} from "../interfaces/Jogador";
+import type { JogadorGet } from '../interfaces/JogadorGet';
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +14,10 @@ export class JogadorService {
 
   cadastrarJogador(jogador: Jogador){
     return this.httpClient.post(this.uri, jogador);
+  }
+
+  resgatarJogadores(){
+    return this.httpClient.get<JogadorGet[]>(this.uri);
   }
 }
   
