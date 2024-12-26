f435b3335c8200c98382e9cc3b35cba5e20ae6c8
Thullyoo <thullyofilho@gmail.com>
2024-12-25 23:48:06 -0300

    feat: apply method onSubmit to register Jogador

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,13 +1,49 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { routes } from './app.routes';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { Router, RouterModule, RouterOutlet } from '@angular/router';
+import { JogadorService } from './services/jogador.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'desafio-uol-front';
+
+  formBuilder = inject(FormBuilder);
+
+  jogadorService = inject(JogadorService);
+
+  router = inject(Router);
+
+  form = this.formBuilder.group({
+    nome: new FormControl('', {}),
+    email: new FormControl('', {}),
+    telefone: new FormControl('', {}),
+    grupoCodinome: new FormControl('', {})
+  });
+
+  onSubmit(event: Event) {
+    event.preventDefault();
+    if (this.form.valid) {
+      this.jogadorService.cadastrarJogador({
+        email: this.form.value.email!,
+        nome: this.form.value.nome!,
+        telefone: this.form.value.telefone!,
+        grupoCodinome: this.form.value.grupoCodinome!
+      }).subscribe({
+        next(value) {
+          console.log(value);
+        },
+        error(err) {
+          console.error(err);
+        },
+      })
+    } else {
+      console.log('Formulário inválido');
+    }
+  }
 }
