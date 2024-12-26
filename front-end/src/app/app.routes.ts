4e870624dbc267f490a461a05bfdbe57ee25d06d
Thullyoo <thullyofilho@gmail.com>
2024-12-26 11:02:33 -0300

    feat: add routes

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { ListarJogadoresComponent } from './pages/listar-jogadores/listar-jogadores.component';
+import { CadastrarJogadoresComponent } from './pages/cadastrar-jogadores/cadastrar-jogadores.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "jogadores",
+    component: ListarJogadoresComponent
+  },
+  {
+    path: "**",
+    component: CadastrarJogadoresComponent
+  }
+];
